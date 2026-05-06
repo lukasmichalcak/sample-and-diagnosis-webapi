a2c8a437db5211f603c40cc1d2c0b4f9fc10c497
lukasmichalcak <xmichalcak@stuba.sk>
2026-05-06 22:39:24 +0200

    Add mongodb compose file with correct SAMPLE_AND_DIAGNOSIS_ prefixed variables

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,10 +10,24 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:SAMPLE_AND_DIAGNOSIS_API_ENVIRONMENT="Development"
 $env:SAMPLE_AND_DIAGNOSIS_API_PORT="8080"
+$env:SAMPLE_AND_DIAGNOSIS_API_MONGODB_USERNAME="root"
+$env:SAMPLE_AND_DIAGNOSIS_API_MONGODB_PASSWORD="neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
     "start" {
+        try {
+            mongo up --detach
         go run ${ProjectRoot}/cmd/sample-and-diagnosis-api-service
+        } finally {
+            mongo down
+        }
+    }
+    "mongo" {
+        mongo up
     }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
